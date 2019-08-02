8f9cbb25473094559cb9b0ff9248d6cd4077c671
Tassa Tenhouse <kmtenhouse@gmail.com>
2019-08-02 01:02:18 -0700

    Added boilerplate for mongoose connection

--- database/index.js
+++ database/index.js
@@ -1,2 +1,18 @@
 "use strict";
 
+const mongoose = require("mongoose"),
+    bluebird = require("bluebird");
+let connect;
+
+module.exports = function () {
+    connect = (config) => {
+        const { databaseRef } = config;
+        // Use bluebird
+        mongoose.Promise = bluebird;
+        return mongoose.connect(databaseRef, { useNewUrlParser: true });
+    }
+
+    return {
+        connect: connect
+    };
+}
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 "use strict";
 
 const server = require("./server")(),
+  database = require("./database")(),
   config = require("./config");
 
+database.connect(config);
 server.create(config);
 server.start();
