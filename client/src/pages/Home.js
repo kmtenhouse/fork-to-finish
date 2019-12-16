17e323ef1958db5c8cd41d545b0aff9f73a7247a
Tassa Tenhouse <kmtenhouse@gmail.com>
2019-12-16 14:38:10 -0800

    Adding custom styling for very small screens

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import "./page.css";
+import "./home.css";
 
 // Note: In development, we want a couple routes to direct to the backend for ouath flow
 const baseURL = (process.env.NODE_ENV === "development" ? "http://localhost:4000" : '');
@@ -7,12 +8,11 @@ class Home extends Component {
 
     render() {
         return (
-            <div className="page">
-
-                <div className="page__section page__section--flex">
+            <div className="page page--home">
+                <div className="page__section page__section--home">
                     <div className="page__subsection page__subsection--teaser">
                         <h1 className="page__subhead">Pick your Passion.</h1>
-                        <p>Save palettes of the most vibrant colors around!  Choose from any of our colors, or select your own.</p>
+                        <p>Save palettes of the most vibrant colors around!  Select your favorite colors and let your creativity ignite. What are you waiting for?</p>
                         <p>
                             <a href={`${baseURL}/auth/google`}>
                                 <button className="page__link page__link--button">
@@ -21,7 +21,7 @@ class Home extends Component {
                             </a>
                         </p>
                     </div>
-                    <div className="page__subsection">
+                    <div className="page__subsection page__subsection--splash">
                         <img className="page__splash" src="/images/swatches.png" alt="Illustration of a color palette" />
                     </div>
                 </div>
