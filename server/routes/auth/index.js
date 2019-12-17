962ce9135cc00fb173a144ea2396f1aeb3a66934
Tassa Tenhouse <kmtenhouse@gmail.com>
2019-12-17 05:26:27 -0800

    Fixing logout

--- server/routes/auth/index.js
+++ server/routes/auth/index.js
@@ -13,26 +13,26 @@ router.get("/", (req, res) => {
 router.get("/logout", async (req, res) => {
   req.logout();
   await req.session.destroy();
-  if(process.env.NODE_ENV === "production") {
+  if (process.env.NODE_ENV === "production") {
     //if we're in prod, route back to the build version of our app
-    return res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  } 
+    return res.sendFile(path.join(__dirname, "../../../client/build/index.html"));
+  }
   //otherwise, route to the client app on localhost
   res.redirect("http://localhost:3000");
 });
 
 router.get("/whoami", (req, res) => {
-  if(req.user) {
+  if (req.user) {
     return res.json({ loggedIn: true });
   }
   res.json({ loggedIn: false });
 })
 
 router.get("/success", (req, res) => {
-  if(process.env.NODE_ENV === "production") {
+  if (process.env.NODE_ENV === "production") {
     //if we're in prod, route back to the build version of our app
     return res.sendFile(path.join(__dirname, "../../../client/build/index.html"));
-  } 
+  }
   //otherwise, route to the client app on localhost
   res.redirect("http://localhost:3000");
 });
