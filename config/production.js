672090029c26bdee7faea83d372ff80e14ad960a
Tassa Tenhouse <kmtenhouse@gmail.com>
2019-12-17 04:20:06 -0800

    Tweaking config for prod

--- config/production.js
+++ config/production.js
@@ -3,7 +3,7 @@
 const productionConfig = {
   hostname: "heroku", 
   port: process.env.PORT,
-  databaseRef: process.env.MONGO_URI,
+  databaseRef: process.env.MONGODB_URI,
   redisURL: process.env.REDIS_URL,
   google_client_id: process.env.GOOGLE_CLIENT_ID,
   google_client_secret: process.env.GOOGLE_CLIENT_SECRET,
